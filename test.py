4376d6b708b68d514a47e1996a2476c11b23d1a2
D-soldier <gclisp_program@yeah.net>
2015-08-30 20:18:08 +0800

    this change is test

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 #coding:utf-8
 print 'hello test'
+name = raw_input('please enput your name>')
+
+for i in range(3):
+    print 'your name is:%s'%name
+
+print 'so,important things say three times!!!'
